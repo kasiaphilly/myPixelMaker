661aa55adaa9d7baa0bb0b7918400ede9e372481
kasiaphilly <kasiaphilly@users.noreply.github.com>
2018-03-08 21:31:39 -0500

    back to changing canvas size
    using jQuery listeners instead of cached values

--- designs.js
+++ designs.js
@@ -6,15 +6,6 @@ const canvas = document.getElementById('pixelCanvas');
 // access the properties of "eraser" button
 const eraser = document.getElementById('eraser');
 
-// submit field size
-$('#fieldSize').on("click", function(event){
-  event.preventDefault();
-  let fieldSq = $('fieldPx').val() + "px";
-  $('tr').css('height', fieldSq);
-  $('td').css('width', fieldSq);
-});
-
-
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').on("click", function(event){
   //Clear the former grid
@@ -25,24 +16,23 @@ $('#sizePicker').on("click", function(event){
   $("table, tr, td").css("background-color", bgColor);
 
 // calculating width and length of canvas and setting it in stylesheet so that it does not deform when we change the size of browser window
-  const tbWidth = height*20 +"px";
+  const tbWidth = $('#inputWidth').val()*20 +"px";
   $("table").css("width", tbWidth);
-  const tbHeight = width*20 +"px";
+  const tbHeight = $('#inputHeight').val()*20 +"px";
   $("table").css("height", tbHeight);
 });
 
 //create Grid function
 function createGrid() {
-  for (let i = 0; i<height; i++) {
-    let row = canvas.insertRow(i);
+  for (let i = 0; i<$('#inputHeight').val(); i++) {
+    const row = canvas.insertRow(i);
     $("tr").addClass("rowCl");
-    for (let j = 0; j<width; j++) {
-      let cell = row.insertCell(j);
+    for (let j = 0; j<$('#inputWidth').val(); j++) {
+      const cell = row.insertCell(j);
       $("td").addClass("cellCl");
     }
   }
 };
-
 
 
 //color cell
